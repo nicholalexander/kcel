19bf8837ce8a15fddec671e7eb15e62b3b49ef35
nichol alexander <nichol.alexander@gmail.com>
2025-10-06 00:52:47 -0400

    cache issue

--- dist/sw.js
+++ dist/sw.js
@@ -2,4 +2,4 @@ const CACHE_NAME='passphrase-v1';
 const urlsToCache=['/','/wordlist.js','/app.js','/about','/why'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(n=>Promise.all(n.map(n=>n!==CACHE_NAME?caches.delete(n):null))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>{if(r)return r;const f=e.request.clone();return fetch(f).then(r=>{if(!r||r.status!==200||r.type!=='basic')return r;const c=r.clone();caches.open(CACHE_NAME).then(a=>a.put(e.request,c));return r})}))});
+self.addEventListener('fetch',e=>{const url=new URL(e.request.url);if(url.pathname.includes('/api/')){e.respondWith(fetch(e.request));return;}e.respondWith(caches.match(e.request).then(r=>{if(r)return r;const f=e.request.clone();return fetch(f).then(r=>{if(!r||r.status!==200||r.type!=='basic')return r;const c=r.clone();caches.open(CACHE_NAME).then(a=>a.put(e.request,c));return r})}))});
